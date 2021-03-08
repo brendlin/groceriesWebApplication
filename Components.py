23d7d3d9ab261531c74086d4e85bfba93817378b
brendlin <kurt.brendlinger@cern.ch>
2021-03-08 15:47:44 +0100

    Improve sync page

--- Components.py
+++ Components.py
@@ -2,16 +2,21 @@
 import dash_html_components as html
 
 sync_button = html.Button(id='sync-button',children='Sync',n_clicks=0,
-                          style={'verticalAlign':'bottom'})
+                          className='button buttonPhone',
+                          style={'verticalAlign':'bottom','background-color':'white'})
 
 sync_button_wrapdiv = html.Div(sync_button,
                                style={'display':'inline-block','verticalAlign':'middle'})
 
 sync_message = html.Div('Don\'t worry: you will only lose the last change you just made.',
-                        style={'marginTop':'10px'})
+                        style={'marginTop':'10px','background-color':'white'})
+
+sync_wrapdiv = html.Div(children=[sync_button_wrapdiv,sync_message],
+                        style={'background-color':'white','padding':'10px 10px'},
+                        )
 
 sync_div = html.Div(id='sync-div', # This is the sync box
-                    children=[sync_button_wrapdiv,sync_message],
+                    children=[sync_wrapdiv],
                     style={
                         'text-align': 'center',
                         'background-color':'rgba(255, 255, 255, 0.6)',
